012d48ff21bace935c3e77d8e8fb4bb823d00e4d
JoyJazper <pjk096@gmail.com>
2018-09-19 16:27:11 +0530

    Created connection between DB and FE

--- Website4k/App1/urls.py
+++ Website4k/App1/urls.py
@@ -4,5 +4,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #/app1/
     path('', views.index,name = 'index' ),
+
+    #/app1/storeId
+    path('<int:storeID>/',views.ProductPanel, name = 'ProductPanel' )
 ]
--- Website4k/App1/views.py
+++ Website4k/App1/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Users, Stores, Products, OrdersList, OrderData
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1> This is the the app homepage</h1>")
+    listStores = Stores.objects.all()
+    html = ''
+    template = loader.get_template('templates/App1/index.html')
+    context = {
+        'Stores': Stores,
+    }
+    for store in listStores:
+        url = '/App1/'+ str(store.id) + '/'
+        html += '<a href="'+url+'">'+ store.storeName+'</a><br />'
+    return HttpResponse(template.render(context, request))
+
+def ProductPanel(request, storeID):
+    return HttpResponse("<h1> This is the Products Panel " + str(storeID) + "</h1>")
